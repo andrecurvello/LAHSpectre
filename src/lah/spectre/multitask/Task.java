3087b27193f5e39ce105e9b7706bc1d90191d880
Vu An Hoa <anhoacorp@gmail.com>
2013-03-12 18:48:10 +0800

    Simplified Task interface Ask ID->Future table to cancel tasks in TaskManager

--- src/lah/spectre/multitask/Task.java
+++ src/lah/spectre/multitask/Task.java
@@ -1,21 +1,30 @@
 package lah.spectre.multitask;
 
-import java.util.concurrent.Future;
-
-import lah.spectre.interfaces.IResult;
+import java.util.concurrent.ExecutorService;
 
 /**
- * General interface for a task
+ * General interface for a task extending {@link Runnable} with a method to
+ * indicate whether an instance is ready for execution; if it is, it can be
+ * submitted to the {@link ExecutorService} for execution.
  * 
  * @author L.A.H.
  * 
  */
-public interface Task {
+public interface Task extends Runnable {
 
-	IResult getResult();
-
-	void setFuture(Future<?> future);
-
-	void start();
+	/**
+	 * Check if this task is executable. This method should be implemented based
+	 * the context (i.e. meaning of the task).
+	 * 
+	 * For example, task can have other tasks as dependency and is only
+	 * executable if their dependencies are met.
+	 * 
+	 * In another context, tasks might require resources to be available such as
+	 * displaying the image requires the image file to be retrieved over the
+	 * network.
+	 * 
+	 * @return {@code true} if this task is executable; {@code false} otherwise.
+	 */
+	boolean isExecutable();
 
 }
--- src/lah/spectre/multitask/TaskManager.java
+++ src/lah/spectre/multitask/TaskManager.java
@@ -1,5 +1,7 @@
 package lah.spectre.multitask;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.ExecutorService;
@@ -7,7 +9,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
- * Generic task manager to ease adding, starting, cancel, ... of tasks
+ * Generic task manager to ease management of tasks such as adding, scheduling,
+ * canceling, etc
  * 
  * @author L.A.H.
  * 
@@ -17,37 +20,65 @@ import java.util.concurrent.Future;
 public class TaskManager<T extends Task> {
 
 	/**
-	 * We use the address of the task to identify it
+	 * List of tasks waiting to be scheduled for execution
 	 */
-	private Map<Integer, T> task_id_table;
+	private List<T> pending_task_queue;
 
 	/**
 	 * An {@link ExecutorService} to schedule execution of tasks in background
 	 */
 	private ExecutorService task_executor;
 
+	/**
+	 * We use the address of the task to identify it
+	 */
+	private Map<Integer, T> task_id_table;
+
+	/**
+	 * Table mapping task ID to {@link Future} objects for cancellation
+	 */
+	private Map<Integer, Future<?>> future_id_table;
+
 	public TaskManager() {
 		task_id_table = new TreeMap<Integer, T>();
+		future_id_table = new TreeMap<Integer, Future<?>>();
 		task_executor = Executors.newSingleThreadExecutor();
+		pending_task_queue = new LinkedList<T>();
+		// TODO start the scheduler thread which periodically
+		// submit executable tasks to the executor
 	}
 
 	public void addTask(final T task) {
 		if (task != null) {
 			task_id_table.put(System.identityHashCode(task), task);
-			Future<?> future = task_executor.submit(new Runnable() {
-
-				@Override
-				public void run() {
-					task.start();
-					System.out.println("Task " + task + " is finished.");
-				}
-			});
-			task.setFuture(future);
 		}
 	}
 
+	public void cancel(T task) {
+		if (task != null) {
+			// remove the task if it is pending
+			pending_task_queue.remove(task);
+
+			// cancel the task if it is executing
+			Future<?> task_future = future_id_table.get(System
+					.identityHashCode(task));
+			if (task_future != null)
+				task_future.cancel(true);
+		}
+	}
+
+	public void enqueue(T task) {
+		pending_task_queue.add(task);
+		// TODO move the remaining code to the scheduler thread
+		// if the task is executable, submit it
+		Future<?> future = task_executor.submit(task);
+		future_id_table.put(System.identityHashCode(task), future);
+	}
+
 	public void removeTask(int id) {
-		task_id_table.remove(id);
+		T task = task_id_table.remove(id);
+		if (task != null)
+			cancel(task);
 	}
 
 }
